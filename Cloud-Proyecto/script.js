eb3d467a4e7a46f2c258cce6213519aef08951dd
angieraga09-lab <angieraga09@gmail.com>
2026-07-20 22:05:24 -0500

    Add files via upload

---
+++ Cloud-Proyecto/script.js
@@ -0,0 +1,48 @@
+function sumar(){
+
+    let n1 = Number(document.getElementById("num1").value);
+
+    let n2 = Number(document.getElementById("num2").value);
+
+    let resultado = n1 + n2;
+
+    document.getElementById("resultado").innerHTML =
+    "Resultado: " + resultado;
+
+}
+
+let contador = 0;
+
+function aumentar(){
+
+    contador++;
+
+    document.getElementById("contador").innerHTML = contador;
+
+}
+
+function disminuir(){
+
+    contador--;
+
+    document.getElementById("contador").innerHTML = contador;
+
+}
+
+function mostrarFecha(){
+
+    let fecha = new Date();
+
+    document.getElementById("fecha").innerHTML =
+    fecha.toLocaleString();
+
+}
+
+function saludar(){
+
+    let nombre = document.getElementById("nombre").value;
+
+    document.getElementById("saludo").innerHTML =
+    "Bienvenido " + nombre + ". Tu proyecto está listo para desplegarse en Vercel o Netlify.";
+
+}
